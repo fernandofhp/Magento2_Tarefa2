3c26bb421580b486cb730835b3f5b8249290c09d
fernandofhp <fhpdev@google.com>
2020-10-01 23:43:01 -0300

    Modulo - OK | Cli command terminal - OK

---
+++ app/code/Fhpdev/Pintura/Console/Command/Paintshot.php
@@ -0,0 +1,89 @@
+<?php
+namespace Fhpdev\Pintura\Console\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Paintshot extends Command
+{
+    const COR = 'cor';
+    const LOJA = 'loja';     
+    
+    public function __construct() {
+        parent::__construct('fhpdev:paintshot');
+    }       
+
+    protected function configure(){        
+        $options = [
+			new InputOption(
+				self::COR,
+				null,
+				InputOption::VALUE_REQUIRED,
+				'cor'
+            ),
+			new InputOption(
+				self::LOJA,
+				null,
+				InputOption::VALUE_REQUIRED,
+				'loja'
+            )
+        ];        
+        $this->setName('fhpdev:paintshot') 
+            ->setDescription('Comando CLI para pintura de botões usando bin/magento for Magento 2')
+                ->setDefinition($options);
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output){          
+        if ($cor = $input->getOption(self::COR)) {
+           // testa se cor tem 6 digitos (2 para cada canal RGB)
+           // e se todos os dígitos são números hexadecimais
+            if(!((strlen($cor) == 6) && (ctype_xdigit($cor)))){ 
+                $output->writeln('<error> ERRO: Formato da cor não aceito </>');
+                $output->writeln('<error> Utilize 6 digitos hexadecimais (HEX color) </>');
+                $output->writeln('<error> Exemplo: 0F5E4D </>');
+                return $this;
+            }
+        } else {
+            $cor = 'CCCCCC'; //Cor padrão cinza RGB (204,204,204)
+        }
+        if ($loja = $input->getOption(self::LOJA)) {    
+            //$repo_lojas = new Lojas();
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $lojas_ = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
+            $tab_lojas = $lojas_->getStores();  // $this->_storeManager
+            //$this->_storeManager->getStore()->getId();
+            // testa se a loja existe 
+            $ids_existentes = '(';
+            $existe = false;
+            //$tab_lojas = $repo_lojas->getList();
+            foreach ($tab_lojas as $reg_loja) {
+                $id_loja = $reg_loja["store_id"];
+                $ids_existentes .= " $id_loja";
+                if($loja == $id_loja){
+                    $existe = true; 
+                    break; //encurtando a história...
+                }
+            }
+            $ids_existentes .= ')';
+            if (!$existe) {
+                $output->writeln("<error> ERRO: A loja de ID: [$loja] não existe </>");
+                $output->writeln('<error> IDs de lojas existententes: </>');
+                $output->writeln("<error>$ids_existentes </>");
+                $output->writeln('<error> Exemplo: 0F5E4D </>');
+                return $this;
+            }
+        } else {
+            $output->writeln("<error> ERRO: ID da Loja não foi especificada </>");
+            $output->writeln("<error> bin/magento fhpdev:paintshot --cor <cor> --loja <id da loja></>");
+        }
+        //Efetua a colorização dos buttons
+        //$this->mudacor($cor, $loja);
+        //Msg no terminal
+        $output->writeln('<info> Sucesso!!! </>');
+        $output->writeln("<info> Todos os botões da loja ID: [$loja] foram pintados com a cor [$cor] </>");              
+        return $this;
+    }
+}
